8adcd10024fe3f2c5620afabd856114e60a111a4
NEOCLID <neosalm20@snu.ac.kr>
2025-09-11 00:10:07 +0900

    feat: Secure credentials and configure release signing

--- config.js
+++ config.js
@@ -2,3 +2,9 @@ const DEV_API = 'http://localhost:3000';
 export const API_IP = __DEV__
   ? (typeof document !== 'undefined' ? DEV_API : 'http://192.168.0.16:3000')
   : 'https://othershorts.com';
+
+export const GOOGLE_CLIENT_IDS = {
+    expoClientId: process.env.EXPO_PUBLIC_EXPO_CLIENT_ID,
+    webClientId: process.env.EXPO_PUBLIC_WEB_CLIENT_ID,
+    androidClientId: process.env.EXPO_PUBLIC_ANDROID_CLIENT_ID,
+};
